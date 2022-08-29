86b524749bc1d861c2604a12b04c310f172a2f78
JRGSoares <ricardoguita86@gmail.com>
2022-08-29 13:05:53 -0300

    Organizando
    o código

--- pregao.py
+++ pregao.py
@@ -6,7 +6,7 @@ from PIL import ImageTk, Image
 from time import strftime
 from model import *
 
-################# cores ###############
+######################### Cores #########################
 
 cor0 = "#2e2d2b"  # Preta
 cor1 = "#feffff"  # branca
@@ -20,19 +20,19 @@ cor8 = "#263238"   # + verde
 cor9 = "#e9edf5"   # + verde
 cor10 = "#336688"  # azul -
 
-# Configuração da Janela
+######################### Configuração da Janela #########################
+
 app = Tk()
 app.title('Pregão')
 app.geometry('810x535')
 
-# Funções
-######## Rélogio########
+######################### Funções #########################
+
 def time(): 
     string = strftime('%H:%M:%S') 
     lbl.config(text = string) 
     lbl.after(1000, time) 
 
-# Listar
 def listar():
     total_soma = []
     
@@ -195,86 +195,130 @@ def excluir():
     except:
         messagebox.showinfo(title='Aviso', message='Favor selecionar um item')
 
-# Frames
+def pesquizar():
+    ...
+
+#########################  Frame de cima ######################### 
+
 f1 = Frame(app, width=810, height=135, relief='flat', bg=cor10)
 f1.place(x=0, y=0)
 
+# Frame esquerdo / Entradas
 fe = Frame(f1, width=260, height=135, relief='flat', bg=cor2)
 fe.place(x=0, y=0)
+
+# Frame centro / Painel
 fp = Frame(f1, width=190, height=75, relief='flat', bg=cor5)
 fp.place(x=270, y=10)
+
+# Frame direito / Botões
 fd = Frame(f1, width=330, height=135, relief='flat', bg=cor10)
 fd.place(x=470, y=0)
+
+#########################  Frame de baixo ######################### 
 
 f2 = Frame(app, width=810, height=400, relief='flat', bg=cor3)
 f2.place(x=0, y=135)
 
+# Frame esquerdo / TreeView
 fle = Frame(f2, width=400, height=900, relief='flat', bg=cor10)
 fle.place(x=10, y=10)
+
+# Frame direito / Calendario, Rélogio, Logo e Author
 fld = Frame(f2, width=245, height=900, relief='flat', bg=cor10)
 fld.place(x=565, y=0)
 
-# Entrada
-lnome = Label(fe, text='Nome:', bg=cor2)
-e_nome = Entry()
-ldescricao = Label(fe, text='Descricao:', bg=cor2)
-e_descricao = Entry()
-ldata = Label(fe, text='Data:', bg=cor2)
-e_data = DateEntry(fe)
-lvalor = Label(fe, text='Valor R$:', bg=cor2)
-e_valor = Entry()
-lpesquisar = Label(fd, text='Pesquisar por nome:', bg=cor10, fg=cor1)
-e_pesquisar = Entry()
+######################### Entrada #########################
 
+# Nome
+lnome = Label(fe, text='Nome:', bg=cor2) 
+e_nome = Entry() 
 lnome.place(x=20, y=10)
 e_nome.place(x=100, y=10)
+
+# Descrição
+ldescricao = Label(fe, text='Descricao:', bg=cor2) 
+e_descricao = Entry()
 ldescricao.place(x=20, y=40)
 e_descricao.place(x=100, y=40)
+
+# Data
+ldata = Label(fe, text='Data:', bg=cor2) 
+e_data = DateEntry(fe)
 ldata.place(x=20, y=70)
 e_data.place(x=100, y=70)
+
+# Valor
+lvalor = Label(fe, text='Valor R$:', bg=cor2) 
+e_valor = Entry()
 lvalor.place(x=20, y=100)
 e_valor.place(x=100, y=100)
+
+# Pesquisar
+lpesquisar = Label(fd, text='Pesquisar por nome:', bg=cor10, fg=cor1) 
+e_pesquisar = Entry()
 lpesquisar.place(x=145, y=20)
 e_pesquisar.place(x=615, y=45)
 
-# Painel
+######################### Painel #########################
+
 total = StringVar()
-texto= Label(fp,text='Total', width=22, height=1, bg=cor2, fg=cor0, font=('Ivy 12 bold'))
+
+# Texto Total
+texto= Label(fp,text='Total', width=22, height=1, bg=cor2, fg=cor0, font=('Ivy 12 bold')) 
 texto.place(x=-10, y=0)
-painel = Label(fp,textvariable=total, width=13, height=1, bg=cor2, fg=cor0, font=('Ivy 20 bold'))
+
+# Valor no Painel
+painel = Label(fp,textvariable=total, width=13, height=1, bg=cor2, fg=cor0, font=('Ivy 20 bold')) 
 painel.place(x=0, y=25)
 
-# Botões
-b1 = Button(fd, text='Cadastrar', width=10, height=1, command=inserir)
+######################### Botões #########################
+
+# Botão Cadastrar
+b1 = Button(fd, text='Cadastrar', width=10, height=1, command=inserir) 
 b1.place(x=0, y=10)
-b2 = Button(fd, text='Atualizar', width=10, height=1, command=confirmar)
+
+# Botão Atualizar
+b2 = Button(fd, text='Atualizar', width=10, height=1, command=confirmar) 
 b2.place(x=0, y=50)
-b3 = Button(fd, text='Deletar', width=10, height=1, command=excluir)
+
+# Botão Deletar
+b3 = Button(fd, text='Deletar', width=10, height=1, command=excluir) 
 b3.place(x=0, y=90)
-b4 = Button(fd, text='Pesquisar', width=10, height=1)
+
+# Botão Pesquisar
+b4 = Button(fd, text='Pesquisar', width=10, height=1) 
 b4.place(x=170, y=80)
 
-# Tree
+######################### TreeView #########################
+
+# Configurando colunas no Treeview
 cols = ('id', 'nome', 'descricao', 'data', 'valor')
 tree = ttk.Treeview(fle, columns=cols, show='headings', height=18)
 
+# Configurando tamanho das colunas no treeview
 tree.column('id', minwidth=0, width=50)
 tree.column('nome', minwidth=0, width=120)
 tree.column('descricao', minwidth=0, width=210)
 tree.column('data', minwidth=0, width=80)
 tree.column('valor', minwidth=0, width=80)
 
+# Configurando cabeçalho no treeview
 tree.heading('id', text='Cod.')
 tree.heading('nome', text='Nome')
 tree.heading('descricao', text='Descrição')
 tree.heading('data', text='Data')
 tree.heading('valor', text='Valor')
 
-vs = ttk.Scrollbar(fle, orient=VERTICAL, command=tree.yview)
-tree.configure(yscroll = vs.set)
-vs.grid(column=1, row=0, sticky='ns')
+# Scrollbar
+vs = ttk.Scrollbar(fle, orient=VERTICAL, command=tree.yview) 
 
+# Setando scrollbar no treeview
+tree.configure(yscroll = vs.set) 
+
+# Mostrar treeview e scrollbar
 tree.grid(column=0, row=0, sticky='ns')
+vs.grid(column=1, row=0, sticky='ns')
 
 # Calendário
 cal = Calendar(fld)
@@ -293,6 +337,7 @@ l.place(x=5, y=230)
 al = Label(fld, text='by ricardoguita86@gmail.com', bg=cor10, fg=cor1, font=('Ivy 12 bold'))
 al.place(x=0,y=370)
 
+# Iniciando loops
 time() 
 listar()
 app.mainloop()
